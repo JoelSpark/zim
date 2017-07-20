e5639718975ab81db3d8b74d659c9f5b1399b269
Joel Spark <Joel Spark>
2017-07-20 15:52:10 +0100

    working development server

--- zim/settings.py
+++ zim/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'zim.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zim',
+        'USER': 'zimadmin',
+        'PASSWORD': 'E2ixSuH6OJ9y1pEdBd2P',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
