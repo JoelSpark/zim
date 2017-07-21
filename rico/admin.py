438a70d2e548de6a10be2f87376bd55d2ffd76a1
Joel Spark <Joel Spark>
2017-07-21 18:43:08 +0100

    added app to admin page

--- rico/admin.py
+++ rico/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Asset, Fault
 
-# Register your models here.
+admin.site.register(Asset)
+admin.site.register(Fault)
