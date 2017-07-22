e960dc86203331dfa94e18822f692571ea676c0d
Joel Spark <Joel Spark>
2017-07-22 12:09:12 +0100

    basic rendering of templates

--- rico/urls.py
+++ rico/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /rico/
     url(r'^$', views.index, name='index'),
-    # /fault/id
-    url(r'^(?P<fault_id>[0-9]+)/$', views.detail, name='detail'),
+    # /rico/id
+    url(r'^faults/$', views.fault_list, name='fault list'),
+    url(r'^faults/(?P<fault_id>[0-9]+)/$', views.fault_detail, name='fault detail'),
 ]
--- rico/views.py
+++ rico/views.py
@@ -5,10 +5,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Fault
+
 
 def index(request):
     return HttpResponse("Hello, world. This is the rico index.")
 
 
-def detail(request, fault_id):
+def fault_list(request):
+    fault_list = Fault.objects.order_by('id')
+    context = {
+        'fault_list': fault_list
+    }
+    return render(request, 'faults/index.html', context)
+
+
+def fault_detail(request, fault_id):
     return HttpResponse("You're looking at fault_id %s" % fault_id)
