ce4a54b7603930f250a0ad1b6a5acae516c9a11d
Joel Spark <Joel Spark>
2017-07-25 18:27:11 +0100

    refactored add_comment to make generic

--- rico/urls.py
+++ rico/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # /rico/id
     url(r'^faults/$', views.fault_list, name='fault_index'),
-    url(r'^faults/(?P<fault_id>[0-9]+)/$', views.fault_detail, name='fault_detail'),
-    url(r'^faults/(?P<fault_id>[0-9]+)/comment/$', views.add_comment_to_fault, name='add_comment_to_fault'),
+    url(r'^faults/(?P<pk>[0-9]+)/$', views.fault_detail, name='fault_detail'),
+    url(r'^(?P<object_name>\w+)s/(?P<pk>[0-9]+)/comment/$', views.add_comment, name='add_comment'),
     url(r'^assets/$', views.asset_list, name='asset_index'),
     url(r'^assets/(?P<asset_id>[0-9]+)/$', views.asset_detail, name='asset_detail'),
-    url(r'^comments$', views.comments, name='comments'),
 ]
